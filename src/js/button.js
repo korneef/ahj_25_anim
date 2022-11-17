507a65bc46069b1dd21b6778907b06eb6a438a35
Valentin Korneev <v.korneef@gmail.com>
2022-11-17 11:47:13 +0600

    Collapse function done

---
+++ src/js/button.js
@@ -0,0 +1,21 @@
+const button = document.querySelector('.collapse-button')
+const textContainer = document.querySelector('.text-container');
+
+button.addEventListener('click', () => {
+    const height = textContainer.offsetHeight;
+    if (textContainer.dataset.open === undefined || textContainer.dataset.open === 'false') {
+        textContainer.dataset.open = 'true'
+        const naturelHeight = textContainer.scrollHeight;
+        textContainer.style.height = `${naturelHeight}px`
+    } else {
+        textContainer.style.height = `${height}px`;
+        textContainer.dataset.open = 'false'
+        setTimeout(() => {textContainer.style.height = `0px`}, 0) 
+    }
+})
+
+textContainer.addEventListener('transitionend', () => {
+    if (textContainer.style.height !== '0px') {
+        textContainer.style.height = 'auto';
+    }
+})
